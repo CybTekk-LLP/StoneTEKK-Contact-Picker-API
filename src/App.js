4a5a7bf878529a8ee2a665dab05bd89023c591a6
grv-saini-20 <gouravsaini9797@gmail.com>
2024-11-24 09:33:54 +0530

    feat: Routes

--- src/App.js
+++ src/App.js
@@ -1,76 +1,23 @@
 import "./App.css";
-import { useState } from "react";
-import Searchbar from "./components/Searchbar/Searchbar";
-import Profile from "./components/Profile/Profile";
-import Typography from "./components/Typography/Typography";
-import InputDefault from "./components/InputDefault/InputDefault";
-import Button from "./components/Button/Button";
-import ContextMenu from "./components/ContextMenu/ContextMenu";
-import Anchor from "./components/Anchor/Anchor";
-import Toast from "./components/Toast/Toast";
-import Select from "./components/Select/Select";
-import Card from "./components/Card/Card";
-import EmptyState from "./components/EmptyState/EmptyState";
-import Footer from "./components/Footer/Footer";
 import AddDetails from "./components/AddDetails/AddDetails";
-import store from "./store/store";
-import { Provider } from "react-redux";
+import AddContacts from "./components/AddContacts/AddContacts";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import RootLayout from "./components/RootLayout/RootLayout";
 
 function App() {
-  const [searchValue, setSearchValue] = useState("");
-  const [inputValue, setInputValue] = useState();
-  const [imgUrl, setImgUrl] = useState("");
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+    <Route index element={<AddDetails/>}/>
+    <Route path="/addcontact" element={<AddContacts/>}/>
+    </Route>
+  ))
 
-  const handleImageUrl = (img) => {
-    setImgUrl(img);
-  };
-
-  const handleInputValue = (value) => {
-    setInputValue(value);
-  };
-
-  const handleSearch = (value) => {
-    setSearchValue(value);
-  };
-
-  const editContact = () => {};
-  const deleteContact = () => {};
   return (
     <>
-    <Provider store={store}>
-    <AddDetails/>
-    </Provider>
+    <RouterProvider router={router} />
 
-      {/* <Footer
-        text={"Experimental Feature for"}
-        url={"https://stonetekk.in"}
-        link={"StoneTEKK.in"}
-      ></Footer> */}
 
-      {/* <Profile getImage={handleImageUrl}></Profile>
-      <Searchbar onSearch={handleSearch} />
-      <Typography text="assdgsdgsd" type="body" _fontweight={300} />
-      <Button
-        text={"haha"}
-        type={"secondary"}
-        textColor={"var(--correct-darker)"}
-      ></Button>
-      <Button text={"haha"} type={"primary"} textColor={"var(--card)"}></Button>
-
-      <Typography text="assdgsdgsd" type="branding" _fontweight={300} />
-      <p>{imgUrl}</p>
-      <InputDefault
-        type="text"
-        _inputMode="text"
-        handleValue={handleInputValue}
-        inputLabel={"name"}
-        index={1}
-        autoComplete={"name"}
-        placeholder={"john"}
-      />
-      <p>{inputValue}</p>
-
-      <ContextMenu
+      {/* <ContextMenu
         text="Edit Contact"
         textDanger="Delete Contact"
         textColor="var(--primary-900)"
@@ -92,11 +39,7 @@ function App() {
         address={"$38 K/123 Triveel Nager sitapur rood lucknow"}
       ></Card>
       <EmptyState text="Add members via filling form or using contact picker if supported"></EmptyState>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br> */}
-    </>
+    </> */}
   );
 }
 
